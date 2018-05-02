f7c6a11bcb791d84ab878f36d56440720468d2aa
ulbricht-inr <chaosdelphin@web.de>
2018-05-02 17:23:16 +0200

    TN_int_template sync from internal repro

--- TN_int_template/push2git.sh
+++ TN_int_template/push2git.sh
@@ -2,7 +2,7 @@
 name="TN_int_template"
 message=${1:-"$name sync from internal repro"}
 gitdir="/home/delphin/svn2/brynhild/InnoRoute/hg_sw/TrustNode/EmbeddedLinux/BuildRootPKGs/gitcontent/packages/$name"
-hgdir="/home/delphin/svn2/brynhild/InnoRoute/hg_sw/TrustNode/EmbeddedLinux/BuildRootPKGs/InnoRoute/$name"
+hgdir="/home/delphin/svn2/brynhild/InnoRoute/trustnode_software/BuildRootPKGs/InnoRoute/$name"
 cd $gitdir
 for file in $(git ls-files); do
 cp $hgdir/$file $gitdir/$file
