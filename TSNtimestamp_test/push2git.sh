36442eec1c17656eecd881c3899c5f7c63feb5b7
ulbricht-inr <chaosdelphin@web.de>
2018-05-28 12:43:19 +0200

    add_ts_test_software

---
+++ TSNtimestamp_test/push2git.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+name="TSNtimestamp_test"
+message=${1:-"$name sync from internal repro"}
+gitdir="/home/delphin/svn2/brynhild/InnoRoute/hg_sw/TrustNode/EmbeddedLinux/BuildRootPKGs/gitcontent/packages/$name"
+hgdir="/home/delphin/svn2/brynhild/InnoRoute/trustnode_software/BuildRootPKGs/InnoRoute/$name"
+cd $gitdir
+for file in $(git ls-files); do
+cp $hgdir/$file $gitdir/$file
+git add $gitdir/$file
+done
+git commit -m"$message"
