356f282bb43b1b097d6d84fec1dd97d908b6c337
ulbricht-inr <chaosdelphin@web.de>
2018-06-22 09:04:03 +0200

    TN_start sync from internal repro

--- TN_start/files/start_ovs.sh
+++ TN_start/files/start_ovs.sh
@@ -3,6 +3,8 @@ rmmod ip_gre
 rmmod gre
 modprobe openvswitch
 /etc/init.d/openvswitch stop
+/usr/share/openvswitch/scripts/ovs-ctl stop
 /etc/init.d/openvswitch start
+/usr/share/openvswitch/scripts/ovs-ctl start
 
 
