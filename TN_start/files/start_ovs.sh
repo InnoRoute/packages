f8831912f013ff043172a1fc0655a48f0688f752
M.Ulbricht <ulbricht@innoroute.de>
2018-09-07 12:04:50 +0200

    TN_start sync from internal repro

--- TN_start/files/start_ovs.sh
+++ TN_start/files/start_ovs.sh
@@ -1,10 +1,13 @@
-#/bin/bash
-rmmod ip_gre
-rmmod gre
-modprobe openvswitch
-/etc/init.d/openvswitch stop
-/usr/share/openvswitch/scripts/ovs-ctl stop
-/etc/init.d/openvswitch start
-/usr/share/openvswitch/scripts/ovs-ctl start
+#!/bin/bash
+echo "#!/bin/bash" > /usr/share/openvswitch/scripts/ovs-kmod-ctl
+chmod +x /usr/share/openvswitch/scripts/ovs-kmod-ctl
+ovs-ctl start
+#rmmod ip_gre
+#rmmod gre
+#modprobe openvswitch
+#/etc/init.d/openvswitch stop
+#/usr/share/openvswitch/scripts/ovs-ctl stop
+#/etc/init.d/openvswitch start
+#/usr/share/openvswitch/scripts/ovs-ctl start
 
 
